d1575cf26022ae8de5cc6e118fd7a50cabc142d0
Patrícia Freitas <104458596+patyfreitasbr@users.noreply.github.com>
2024-09-10 18:02:57 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-// arrays, objetos
 let meta = {
     value: 'ler um livro por mês',
-    checked: false,
-    log: (info) => {
-        console.log(info)
-    }   
+    checked: true,    
+}  
+    
+ let metas = [
+     meta,
+    {
 
-}
+        value: "camihar 20 minutos todos os dias",
+        checked: false
+    }
+]
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value)
 
+console.log(metas[1].value)
 
-//function //arrow function
-
-//const criarMeta = () => {}
-
-//function criarMeta () {}
 
